9d03fb8f15cfaaae7437f7e01c4be7854861d06b
IsHanpi <144578490+IsHanpi@users.noreply.github.com>
2023-12-10 16:30:21 +0800

    Add files via upload

---
+++ lab3.3/lab3.3/3.3.cpp
@@ -0,0 +1,12 @@
+﻿#include "mytemperature.h"
+using namespace std;
+int main() {
+	double cel,fah;
+	cout << "Celsius    Fahrenheit";
+	cin >> cel;
+	printf("%.1f %.1f\n", cel, celsius_to_fah(cel));
+	cout << "Fahrenheit    Celsius";
+	cin >> fah;
+	printf("%.1f %.2f\n", fah, fahrenheit_to_cels(fah));
+	return 0;
+}
---
+++ lab3.3/lab3.3/mytemperature.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+double celsius_to_fah(double cel) {
+	double f;
+	f = cel * 1.8 + 32;
+	return f;
+}
+double fahrenheit_to_cels(double fah) {
+	double c;
+	c = (fah - 32) / 1.8;
+	return c;
+}
